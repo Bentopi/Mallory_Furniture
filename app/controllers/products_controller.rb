0b9aaed1299f57287b9d3ec373559b9c6b2c6875
Brian Simmons <bsimmonsn@gmail.com>
2016-06-19 18:38:05 -0500

    full commit, still have some image issues

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ require 'csv'
 class ProductsController < ApplicationController
     def list
         @product_list = fetch_products
+        product = @product_list.find { |p| p.id == params[:id] }
     end
     def detail
         product_list = fetch_products
